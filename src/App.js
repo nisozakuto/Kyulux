8d56f07f6a03401e51f7c15848e734b35f5c3857
nisozakuto <nisozakuto@gmail.com>
2021-02-06 23:03:47 -0500

    Header added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myData: "",
+    };
+  }
+  componentDidMount() {
+    this.fecthData();
+  }
+  fecthData() {
+    fetch(`https://api.fda.gov/drug/drugsfda.json?limit=1`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ myData: data });
+      });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
 }
-
-export default App;
