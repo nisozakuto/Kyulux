aece630c2d517b8e65ef73f854798af218034ed9
nisozakuto <nisozakuto@gmail.com>
2021-02-06 23:29:40 -0500

    Added a few buttons to filter and query the data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import "./App.css";
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      myData: "",
+      searchResults: "",
     };
   }
   componentDidMount() {
     this.fecthData();
   }
   fecthData() {
-    fetch(`https://api.fda.gov/drug/drugsfda.json?limit=1`, {
-      method: "GET",
-    })
+    fetch(
+      `https://api.fda.gov/drug/drugsfda.json?search=products.marketing_status:"Discontinued"`,
+      {
+        method: "GET",
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
-        this.setState({ myData: data });
+        this.setState({ searchResults: data });
       });
   }
 
@@ -24,6 +28,22 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        <div className="container">
+          <section className="marketingStatusSection">
+            <h3>Marketing Status</h3>
+            <aside className="marketingStatus">
+              <button>Discontinued</button>
+              <button>Prescription</button>
+              <button>None (Tentative Approval)</button>
+              <button>Over-the-counter</button>
+            </aside>
+          </section>
+          {this.state.searchResults && this.state.searchResults.results ? (
+            <p>Data</p>
+          ) : (
+            <p>no Data</p>
+          )}
+        </div>
       </div>
     );
   }
