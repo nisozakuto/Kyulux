27a8e95b6f8d455a8c52c7ccd14569a955d587ca
nisozakuto <nisozakuto@gmail.com>
2021-02-07 21:25:45 -0500

    Getting the sponsor name

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import "./App.css";
+
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       searchResults: "",
+      topLevelSelection: "",
+      limit: 99,
+      route: "",
+      marketing_status: "",
     };
   }
-  componentDidMount() {
-    this.fecthData();
-  }
-  fecthData() {
+  componentDidMount() {}
+  fecthData(topLevelSelection) {
     fetch(
-      `https://api.fda.gov/drug/drugsfda.json?search=products.marketing_status:"Discontinued"`,
+      `http://api.fda.gov/drug/drugsfda.json?search=products.route:topical&limit=${this.state.limit}`,
       {
         method: "GET",
       }
@@ -24,19 +27,80 @@ export default class App extends Component {
       });
   }
 
+  // SponsorButton = () => {
+  //   this.setState({
+  //     topLevelSelection: "sponsor",
+  //   });
+  // };
+
+  // MarketingButton = () => {
+  //   this.setState({
+  //     topLevelSelection: "marketing",
+  //   });
+  //   this.fecthData(this.state.topLevelSelection);
+  // };
+
+  route = (e) => {
+    console.log(e);
+    fetch(
+      `https://api.fda.gov/drug/drugsfda.json?search=products.marketing_status:${e}&limit=${this.state.limit}`,
+      {
+        method: "GET",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ marketing_status: data });
+      });
+  };
+
   render() {
     return (
       <div>
         <Header />
         <div className="container">
+          <section className="top-level">
+            {/* <button onClick={this.SponsorButton}>Sponsor by Route</button>
+            <button onClick={this.MarketingButton}>Marketing Status</button> */}
+          </section>
           <section className="marketingStatusSection">
             <h3>Marketing Status</h3>
             <aside className="marketingStatus">
-              <button>Discontinued</button>
-              <button>Prescription</button>
-              <button>None (Tentative Approval)</button>
-              <button>Over-the-counter</button>
+              <button
+                value="Discontinued"
+                onClick={(e) => this.route(e.target.value)}
+              >
+                Discontinued
+              </button>
+              <button
+                value="Prescription"
+                onClick={(e) => this.route(e.target.value)}
+              >
+                Prescription
+              </button>
+              <button
+                value="None (Tentative Approval)"
+                onClick={(e) => this.route(e.target.value)}
+              >
+                None (Tentative Approval)
+              </button>
+              <button
+                value="Over-the-counter"
+                onClick={(e) => this.route(e.target.value)}
+              >
+                Over-the-counter
+              </button>
             </aside>
+          </section>
+          <section>
+            {this.state.marketing_status.results ? (
+              this.state.marketing_status.results.map((e) => {
+                return <p key={e.brand_name}>{e.sponsor_name}</p>;
+                // console.log(e);
+              })
+            ) : (
+              <p>nOthign</p>
+            )}
           </section>
           {this.state.searchResults && this.state.searchResults.results ? (
             <p>Data</p>
