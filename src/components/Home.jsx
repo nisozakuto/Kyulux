e0088018d3c1f1b74dd057718bb8e4101e0791f0
nisozakuto <nisozakuto@gmail.com>
2021-02-08 22:09:51 -0500

    Added some desc and styling to home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,17 @@ import React, { Component } from "react";
 export default class Home extends Component {
   render() {
     return (
-      <main>
+      <main className="home">
         <h1>Welcome to Kylux take away test page</h1>
+        <p>
+          This is a webapp where you can browse some of parts of the open FDA's
+          dataset. I used the{" "}
+          <a href="https://open.fda.gov/apis/drug/drugsfda/example-api-queries/">
+            Drugs@FDA
+          </a>{" "}
+          endpoint. Please feel free to browse around the app and try different
+          features.
+        </p>
       </main>
     );
   }
